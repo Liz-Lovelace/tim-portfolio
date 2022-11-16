a1db0f418eb29c0eabe89057599ac83f61a77664
liz-lovelace <ivanov0604041@gmail.com>
2022-11-17 01:35:19 +0600

    fixed sizing on mobile, I think

---
+++ app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title></title>
+      <meta content="width=device-width" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
